e1b3509e91f84cbae040a0498456ee4d6a50f4bc
jchasehuskey <jchase.huskey@gmail.com>
2022-05-03 07:27:25 -0600

    Updated script.js,added fetch polyfill, added promise polyfill
    Updated script.js:
    
    added URL to API and linked it by fetching with function loadList().  Used Promises inside of loadList that returned response.json.  Used promise to deliver forEach function to display pokemon name & details.
    
    Also added loadDetails function to fetch details from the url.
    
    Updated and wrapped forEach function outside of IIFE function in another function pokemonRepository.loadList().then(function(){.
    
    Added fetch.polyfill.js and promise-polyfills.js to account for browser support.

--- js/script.js
+++ js/script.js
@@ -1,31 +1,19 @@
 let pokemonRepository = (function () {
-  let repository = [
-    { name: 'Charzard', height: 0.6, type: ['Blaze', 'Solar-Power'] },
-
-    { name: 'Onix', height: 8.8, type: ['Sturdy', 'Rock-head', 'Weak-armor'] },
-
-    {
-      name: 'Persion',
-      height: 0.5,
-      type: ['Limber', 'Technician', 'Unnverve'],
-    },
-
-    { name: 'Squirtle', height: 0.5, type: ['Rain-dish', 'Torrent'] },
-
-    { name: 'Mew', height: 0.4, type: ['Synchronize'] },
-  ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add(pokemon) {
-    repository.push(pokemon);
+    pokemonList.push(pokemon);
   }
 
   function getAll() {
-    return repository;
+    return pokemonList;
   }
 
-  let showDetails = function (pokemon) {
-    console.log(pokemon);
-  };
+  // let showDetails = function (pokemon) {
+  //   console.log(pokemon);
+  // };
+  //the function above is listed before the return below
 
   function addListItem(pokemon) {
     let pokemonList = document.querySelector('.pokemon-list');
@@ -36,8 +24,50 @@ let pokemonRepository = (function () {
     listPokemon.appendChild(button);
     pokemonList.appendChild(listPokemon);
     button.addEventListener('click', function (event) {
-      let target = event.target;
-      showDetails(pokemon.name);
+      // let target = event.target;
+      showDetails(pokemon);
+    });
+  }
+
+  function loadList() {
+    return fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url,
+          };
+          add(pokemon);
+          console.log(pokemon); //this is optional to log to console
+        });
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (details) {
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
+
+  function showDetails(pokemon) {
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
     });
   }
 
@@ -45,14 +75,19 @@ let pokemonRepository = (function () {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails,
   };
 })();
 
 console.log(pokemonRepository.getAll());
 
-pokemonRepository.add({ name: 'Chase' });
-console.log(pokemonRepository.getAll());
+// pokemonRepository.add({ name: 'Chase' });
+// console.log(pokemonRepository.getAll());
 
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
