e68c81294f0fd4674e6b9c19cc9f982e48c1b4f1
jchasehuskey <jchase.huskey@gmail.com>
2022-05-17 19:28:54 -0600

    Updated script.js and css stylesheet
    Created and appended new modal and all modal elements to the index. Added for EachLoop inside of typeElement to display all pokemon types on modal.
    
    Added 'is-visible' class to modal to allow for it to be hidden in CSS and applied window eventlisteners to close the modal (including the escape kley)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let pokemonRepository = (function () {
+  //POKEMON LIST ARRAY
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let modalContainer = document.querySelector('#modal-container');
 
   function add(pokemon) {
     pokemonList.push(pokemon);
@@ -9,11 +11,6 @@ let pokemonRepository = (function () {
   function getAll() {
     return pokemonList;
   }
-
-  // let showDetails = function (pokemon) {
-  //   console.log(pokemon);
-  // };
-  //the function above is listed before the return below
 
   function addListItem(pokemon) {
     let pokemonList = document.querySelector('.pokemon-list');
@@ -68,8 +65,79 @@ let pokemonRepository = (function () {
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
       console.log(pokemon);
+      showModal(pokemon);
     });
   }
+
+  // MODAL
+
+  //  function showModal(title, text)
+  function showModal(pokemon) {
+    modalContainer.innerHTML = '';
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    // creating an x button that closes the modal window when clicked
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = 'name: ' + pokemon.name.toUpperCase();
+
+    let heightElement = document.createElement('p');
+    heightElement.innerText = 'height: ' + pokemon.height;
+
+    let typeElement = document.createElement('p');
+    typeElement.innerText = 'Type: ';
+    pokemon.types.forEach((type, numberOfTypes) => {
+      numberOfTypes = pokemon.types.pokemon;
+      if (numberOfTypes === 1) {
+        typeElement.innerText += type.type.name;
+      } else {
+        typeElement.innerText += type.type.name + ' ';
+      }
+    });
+
+    // creating an img tag that pulls front image of pokemon from api
+    let imgElement = document.createElement('img');
+    imgElement.src = pokemon.imageUrl;
+
+    // appending everything in the modal
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(imgElement);
+    modal.appendChild(typeElement);
+    modal.appendChild(heightElement);
+
+    //appending modal to modal container
+    modalContainer.appendChild(modal);
+
+    //makes modal appear
+    modalContainer.classList.add('is-visible');
+  }
+
+  //hides the modal
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  }
+
+  //listens for a keydown - escape key and then calls hideModal function
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  //listens for click outside the modal window
+
+  modalContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
 
   return {
     add: add,
@@ -77,14 +145,10 @@ let pokemonRepository = (function () {
     addListItem: addListItem,
     loadList: loadList,
     loadDetails: loadDetails,
-    showDetails: showDetails,
   };
 })();
 
 console.log(pokemonRepository.getAll());
-
-// pokemonRepository.add({ name: 'Chase' });
-// console.log(pokemonRepository.getAll());
 
 pokemonRepository.loadList().then(function () {
   pokemonRepository.getAll().forEach(function (pokemon) {
